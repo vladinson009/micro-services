5f361b6d3cb35459a8dedcb9a3df9484e2bbeaee
Vladimir Gulev <vladinson009@icloud.com>
2026-06-24 22:46:17 +0200

    init event-bus

---
+++ event-bus/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+app.use(express.json());
+
+app.post('/events', (req, res) => {
+  const event = req.body;
+  console.log(event);
+
+  axios
+    .post('http://localhost:4000/events', event)
+    .catch((res) => console.log('ERR PORT 400'));
+  axios
+    .post('http://localhost:4001/events', event)
+    .catch((res) => console.log('ERR PORT 4001'));
+  axios
+    .post('http://localhost:4002/events', event)
+    .catch((res) => console.log('ERR PORT 4002'));
+
+  res.send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+  console.log('Event bus on PORT: 4005');
+});
