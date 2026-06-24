0870775d5c9e258d2b31363e62dcd290b565fcf7
Vladimir Gulev <vladinson009@icloud.com>
2026-06-24 22:45:58 +0200

    init query

---
+++ query/index.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import cors from 'cors';
+
+type Posts = {
+  [key: string]: {
+    id: string;
+    title: string;
+    comments: { id: string; content: string }[];
+  };
+};
+
+const posts: Posts = {};
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(posts);
+
+  res.send({});
+});
+
+app.listen(4002, () => {
+  console.log('query on PORT: 4002');
+});
