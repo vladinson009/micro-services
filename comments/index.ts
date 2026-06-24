0bd4f21d8a365d650bddd769ffb383760cf4f58a
Vladimir Gulev <vladinson009@icloud.com>
2026-06-24 22:46:35 +0200

    add req

--- comments/index.ts
+++ comments/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { randomBytes } from 'crypto';
 import cors from 'cors';
+import axios from 'axios';
 const commentsByPostId: Record<string, { id: string; content: string }[]> = {};
 
 const app = express();
@@ -11,14 +12,29 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
+
   res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+  res.send({});
 });
 
 app.listen(4001, () => {
